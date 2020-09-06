419460b6b42450905bff98f5fe814f04c2dd1219
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-06 21:16:26 +0300

    Can draw on picture

---
+++ PictureAnalysisEx/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ChangeColorButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LoadPicture = New System.Windows.Forms.Button()
+        Me.LineButton = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ChangeColorButton
+        '
+        Me.ChangeColorButton.Location = New System.Drawing.Point(165, 389)
+        Me.ChangeColorButton.Name = "ChangeColorButton"
+        Me.ChangeColorButton.Size = New System.Drawing.Size(130, 49)
+        Me.ChangeColorButton.TabIndex = 0
+        Me.ChangeColorButton.Text = "Change Color"
+        Me.ChangeColorButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(184, 7)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(443, 376)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'LoadPicture
+        '
+        Me.LoadPicture.Location = New System.Drawing.Point(12, 389)
+        Me.LoadPicture.Name = "LoadPicture"
+        Me.LoadPicture.Size = New System.Drawing.Size(128, 49)
+        Me.LoadPicture.TabIndex = 2
+        Me.LoadPicture.Text = "Load"
+        Me.LoadPicture.UseVisualStyleBackColor = True
+        '
+        'LineButton
+        '
+        Me.LineButton.Location = New System.Drawing.Point(314, 389)
+        Me.LineButton.Name = "LineButton"
+        Me.LineButton.Size = New System.Drawing.Size(130, 49)
+        Me.LineButton.TabIndex = 3
+        Me.LineButton.Text = "Draw Line"
+        Me.LineButton.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(467, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 49)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Draw Circle"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LineButton)
+        Me.Controls.Add(Me.LoadPicture)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ChangeColorButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ChangeColorButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LoadPicture As Button
+    Friend WithEvents LineButton As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ PictureAnalysisEx/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Drawing.Imaging
+Imports AForge.Imaging
+
+Public Class Form1
+    Private Sub ChangeColorButton_Click(sender As Object, e As EventArgs) Handles ChangeColorButton.Click
+        Dim bmp As Bitmap = New Bitmap(PictureBox1.Image)
+        Dim x As Integer
+        Dim y As Integer
+        For x = 0 To bmp.Width - 1
+
+            For y = 0 To bmp.Height - 1
+                Dim oldPixelColor As Color = bmp.GetPixel(x, y)
+                Dim newPixelColor As Color = Color.FromArgb(0, oldPixelColor.G, 0)
+                bmp.SetPixel(x, y, newPixelColor)
+            Next
+
+        Next
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub LoadPicture_Click(sender As Object, e As EventArgs) Handles LoadPicture.Click
+        Dim openfile As New OpenFileDialog
+        If openfile.ShowDialog = Windows.Forms.DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(openfile.FileName)
+        End If
+    End Sub
+
+    Private Sub LineButton_Click(sender As Object, e As EventArgs) Handles LineButton.Click
+        Dim G As Graphics = PictureBox1.CreateGraphics
+        Dim P As Pen = New Pen(Brushes.AliceBlue, 10)
+        G.DrawLine(P, New Point(0, 0), New Point(PictureBox1.Width, PictureBox1.Height))
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim p As New Pen(Color.BlueViolet, 5)
+        Dim g As Graphics
+        g = PictureBox1.CreateGraphics
+        g.DrawEllipse(p, 70, 10, 100, 150)
+    End Sub
+End Class
